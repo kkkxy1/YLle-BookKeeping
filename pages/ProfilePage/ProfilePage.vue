e879a6a78567a2c13d0ad26c7a4ec6b71093e753
linshule <linshule@foxmail.com>
2025-07-26 17:55:14 +0800

    完成我的界面信息栏

---
+++ pages/ProfilePage/ProfilePage.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<view class="avatar-profile">
+			<iui-avatar :size="72" :avatarName="userName" :avatarDesc="Desription">
+				<template #avatarName>
+					<text style="font-size: 16px;">{{userName}}</text>
+				</template>
+				<template #avatarDesc>
+					<text style="font-size: 10px;">{{Desription}}</text>
+				</template>
+			</iui-avatar>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from 'vue';
+
+	const userName = ref("请输入昵称");
+	const userID = ref("123456789");
+	const Desription = ref("用户ID :" + userID.value);
+</script>
+
+<style lang="scss"> 
+.avatar-profile{
+	background-color: white;
+	padding: 20px;
+}
+</style>
