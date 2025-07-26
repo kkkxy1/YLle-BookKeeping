9c5eb779980ffc010a37288319dc6d00ac2e90cc
kxy <3293957379@qq.com>
2025-07-26 21:14:04 +0800

    kxy-钱包页面部分跳转

--- pages/Purse/Purse.vue
+++ pages/Purse/Purse.vue
@@ -9,27 +9,27 @@
 	</view>
 	
 	<uni-section>
-		<uni-card class="custom-card" title="现金" sub-title="现金账户" extra="￥：0.00"  thumbnail="/static/xianjin.jpg">
+		<uni-card class="custom-card" title="现金" sub-title="现金账户" extra="￥：0.00"  thumbnail="/static/xianjin.jpg" @click="gotoAccount('cash')">
 		</uni-card>
  	</uni-section>
 	
 	<uni-section>
-		<uni-card title="储蓄卡" sub-title="储蓄卡账户" extra="￥：0.00"  thumbnail="/static/chuxuka.jpg">
+		<uni-card title="储蓄卡" sub-title="储蓄卡账户" extra="￥：0.00"  thumbnail="/static/chuxuka.jpg" @click="gotoAccount('savings')">
 		</uni-card>
 	</uni-section>
 	
 	<uni-section>
-		<uni-card title="信用卡" sub-title="信用卡账户" extra="待还 ￥：0.00"  thumbnail="/static/xinyongka.jpg">
+		<uni-card title="信用卡" sub-title="信用卡账户" extra="待还 ￥：0.00"  thumbnail="/static/xinyongka.jpg" @click="gotoAccount('credit')">
 		</uni-card>
 	</uni-section>
 	
 	<uni-section>
-		<uni-card title="支付宝" sub-title="网络账户" extra="￥：0.00"  thumbnail="/static/zhifubao.jpg">
+		<uni-card title="支付宝" sub-title="网络账户" extra="￥：0.00"  thumbnail="/static/zhifubao.jpg" @click="gotoAccount('alipay')">
 		</uni-card>
 	</uni-section>
 	
 	<uni-section>
-		<uni-card title="微信" sub-title="网络账户" extra="￥：0.00"  thumbnail="/static/weixin.jpg">
+		<uni-card title="微信" sub-title="网络账户" extra="￥：0.00"  thumbnail="/static/weixin.jpg" @click="gotoAccount('wechat')">
 		</uni-card>
 	</uni-section>
 	
@@ -52,6 +52,36 @@
 <script setup>
     import { ref } from 'vue';
 	
+	const gotoAccount = (type) => {
+		let url = '';
+		
+		switch(type) {
+			case 'cash':
+				url = '/pages/Purse/Xianjin/Xianjin';
+				break;
+				
+			case 'savings':
+			    url = '/pages/Purse/Chuxuka/Chuxuka';
+			    break;
+			
+			case 'credit':
+			    url = '/pages/Purse/Xinyongka/Xinyongka';
+			    break;
+				
+			case 'alipay':
+			    url = '/pages/Purse/Zhifubao/Zhifubao';
+			    break;	
+			 
+			case 'wechat':
+			    url = '/pages/Purse/Wechat/Wechat';
+			    break;	
+		}
+		
+		// 执行页面跳转
+		uni.navigateTo({
+			url: url
+		});
+	}
 
 </script>
 
